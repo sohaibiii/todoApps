a1ac6b055b32523bb7056c8edc99607844293fa5
sohaibiii <bsaif281@gmail.com>
2020-02-17 00:44:45 +0500

    dfsd

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import NavContainer from './src/navigation/RootNavigation';
 import withProvider from './src/redux/withProvider';
 
 const App = () => {
+  useEffect(() => {
+    RNCalendarEvents.authorizationStatus();
+    RNCalendarEvents.authorizeEventStore();
+  }, []);
+
   console.disableYellowBox = true;
 
   return (
